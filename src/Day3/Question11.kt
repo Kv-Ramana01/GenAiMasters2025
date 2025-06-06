35b331d04c77d3076f05db9c719e5f20249a7f92
Kv-Ramana01 <kv42216@gmail.com>
2025-06-06 14:31:59 +0530

    Day3 Question11 Completed

---
+++ src/Day3/Question11.kt
@@ -0,0 +1,9 @@
+package Day3
+
+fun main(){
+    var strengthLevel = 0
+    while (strengthLevel <= 80) {
+        println("Brewing... strength: $strengthLevel")
+        strengthLevel += 10
+    }
+}
