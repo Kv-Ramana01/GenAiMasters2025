e098fe22f18952c97d5917dd8cc1dab9ed370b2d
Kv-Ramana01 <kv42216@gmail.com>
2025-06-08 23:01:33 +0530

    Day3 Question20 Completed

---
+++ src/Day4/Q1.kt
@@ -0,0 +1,15 @@
+package Day4
+
+fun kaffm(kaffe: String) : String {
+    return "Enjoy your $kaffe!"
+}
+
+fun main(){
+    print("Enter a coffee type: ")
+    val kaffe = readLine()
+    if (!kaffe.isNullOrBlank()) {
+        println(kaffm(kaffe))
+    } else {
+        println("No coffee type entered.")
+    }
+}
