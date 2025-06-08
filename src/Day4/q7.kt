ff3bc425939ffdf549eb5e0d73acdfaae40330ec
Kv-Ramana01 <kv42216@gmail.com>
2025-06-09 00:01:17 +0530

    Day4 q7 completed!

---
+++ src/Day4/q7.kt
@@ -0,0 +1,9 @@
+package Day4
+
+fun prepareCoffee(type: String, sugarLevel: Int = 1) {
+    println("Preparing $type with $sugarLevel spoon(s) of sugar.")
+}
+
+fun main(){
+    prepareCoffee("latte")
+}
